f431636f7013c0f43cc78be1555188dd34bbf76a
Kevin Funez <kevin.funez315@gmail.com>
2021-09-09 14:41:22 -0700

    show next astroid
    Co-authored-by: Art Weinstein <artur.weinstein@gmail.com>

--- src/main.js
+++ src/main.js
@@ -25,7 +25,9 @@ $(document).ready(function() {
   function getElements(response) {
     $('.displayPicture').html(`<img src=" ${response.url}">`);
   }
+  let count = 0;
   $('#submit').click(function() {
+    count = 0;
     const start = $('#start').val();
     // const end = $('#end').val();
     // $('#start').val("");
@@ -41,13 +43,13 @@ $(document).ready(function() {
     request.open("GET", url, true);
     request.send();
     function getElements(response) {
-      $('.name').html(`Asteroid name: ${response.near_earth_objects[start][0].name}`);
-      $('.diameter').html(`Estimated Diameter in Feet: ${Math.floor(response.near_earth_objects[start][0].estimated_diameter.feet.estimated_diameter_max)}`);
-      $('.hazard').html(`Is hazardous: ${response.near_earth_objects[start][0].is_potentially_hazardous_asteroid}`);
-      $('.velocity').html(`Velocity: ${Math.floor(response.near_earth_objects[start][0].close_approach_data[0].relative_velocity.miles_per_hour)} MPH`);
-      $('.link').html(`<a href =" ${response.near_earth_objects[start][0].nasa_jpl_url}">Additional Information</a>`);
+      $('.name').html(`Asteroid name: ${response.near_earth_objects[start][count].name}`);
+      $('.diameter').html(`Estimated Diameter in Feet: ${Math.floor(response.near_earth_objects[start][count].estimated_diameter.feet.estimated_diameter_max)}`);
+      $('.hazard').html(`Is hazardous: ${response.near_earth_objects[start][count].is_potentially_hazardous_asteroid}`);
+      $('.velocity').html(`Velocity: ${Math.floor(response.near_earth_objects[start][count].close_approach_data[0].relative_velocity.miles_per_hour)} MPH`);
+      $('.link').html(`<a href =" ${response.near_earth_objects[start][count].nasa_jpl_url}">Additional Information</a>`);
     }
-
+    $(".showButton").show();
     try {
       const isDateValid = checkDate(start);
       if (isDateValid instanceof Error){
@@ -61,6 +63,32 @@ $(document).ready(function() {
       console.error(`Red alert! There's an error! ${error.message}`);
       $('#displayDate').text("please enter a date >:(");
     }
-
+  });
+  $('#next').click(function() {
+    count += 1;
+    const start2 = $('#start').val();
+    let request = new XMLHttpRequest();
+    const url2 = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start2}&end_date=${start2}&api_key=${process.env.API_KEY}`;
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        let total = response.near_earth_objects[start2].length;
+        if (total > count){
+          count + 1;
+        } else {
+          count = 0;
+        }
+        getElements(response);
+      }
+    };
+    request.open("GET", url2, true);
+    request.send();
+    function getElements(response) {
+      $('.name').html(`Asteroid name: ${response.near_earth_objects[start2][count].name}`);
+      $('.diameter').html(`Estimated Diameter in Feet: ${Math.floor(response.near_earth_objects[start2][count].estimated_diameter.feet.estimated_diameter_max)}`);
+      $('.hazard').html(`Is hazardous: ${response.near_earth_objects[start2][count].is_potentially_hazardous_asteroid}`);
+      $('.velocity').html(`Velocity: ${Math.floor(response.near_earth_objects[start2][count].close_approach_data[0].relative_velocity.miles_per_hour)} MPH`);
+      $('.link').html(`<a href =" ${response.near_earth_objects[start2][count].nasa_jpl_url}">Additional Information</a>`);
+    }
   });
 });
